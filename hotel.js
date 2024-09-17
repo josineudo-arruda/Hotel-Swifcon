b266d72b0182822822e0ac2c707e887b39c93ca0
Josineudo Arruda <josineudo.arruda@gmail.com>
2024-09-17 14:49:54 -0300

    add: commit inicial

---
+++ hotel.js
@@ -0,0 +1,48 @@
+
+function inicio() {
+
+    var escolha = parseInt(prompt('Selecione uma opção 1.) Reserva de Quartos 2.) Cadastro de Hóspedes 3.) Abastecimento de Carros 4.) Sair'));
+
+    if (escolha === 1) {
+        reserva_quartos();
+    } else if (escolha === 2) {
+        cadastro_hospedes();
+    } else if (escolha === 3) {
+        abastecer_carros();
+    } else if (escolha === 4) {
+        sair();
+    } else {
+        erro();
+    }
+}		
+
+function reserva_quartos() {
+    alert('HOTEL {NOME DO HOTEL} - RESERVA DE QUARTOS');
+    inicio();
+}
+
+function cadastro_hospedes() {
+    alert('HOTEL {NOME DO HOTEL} - CADASTRO DE HÓSPEDES');
+    inicio();
+}
+
+function abastecer_carros() {
+    alert('HOTEL {NOME DO HOTEL} - ABASTECER');
+    inicio();
+}
+
+function erro() {
+    alert('Por favor, informe um número entre 1 e 4');
+    inicio();
+}
+
+function sair() {
+    var confirma = confirm('Você deseja sair?');
+    if (confirma) {
+        window.close();
+    } else {
+        inicio();
+    }
+}
+
+inicio();
